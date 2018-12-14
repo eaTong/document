0e0b7a58e335f8f93ec572abb1ede2a896bed862
eaTong <eatongchou@gmail.com>
2018-12-14 09:42:37 +0800

    change editor form react-quill to ueditor

--- components/Editor.js
+++ components/Editor.js
@@ -11,8 +11,7 @@ class Editor extends Component {
 
     this.setState({mounted: true, value: this.props.value, unionKey: `editor-container-${Math.random()}`});
     const bdPath = '/ueditor/';
-    createScript(bdPath + 'ueditor.config.js')
-      .then(() => createScript(bdPath + 'ueditor.all.js'))
+    createScript(bdPath + 'ueditor.all.js')
       .then(() => createScript(bdPath + 'lang/zh-cn/zh-cn.js'))
       .then(() => {
         const width = findDOMNode(this.container).offsetWidth;
